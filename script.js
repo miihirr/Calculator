5da53eebd82381a11bd32e6659e87c9b6a73be6b
Mihir <pmihir2001@gmail.com>
2023-11-23 16:58:49 -0500

    create operate function

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
-let add=(a,b)=>a+b;
-let subtract=(a,b)=>a-b;
-let multiply=(a,b)=>a*b;
-let divide=(a,b)=>a/b;
+const add=(a,b)=>a+b;
+const subtract=(a,b)=>a-b;
+const multiply=(a,b)=>a*b;
+const divide=(a,b)=>a/b;
+
+let firstNum;
+let secondNum;
+let operator;
+
+const operate=(operator,firstNum,secondNum)=>{
+    if(operator==="+"){
+        add(firstNum,secondNum);
+    }
+    if(operator==="-"){
+        subtract(firstNum,secondNum);
+    }
+    if(operator==="*"){
+        multiply(firstNum,secondNum);
+    }
+    if(operator==="/"){
+        divide(firstNum,secondNum);
+    }
+}
+
